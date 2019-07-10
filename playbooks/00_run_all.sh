f50db579c0a2cadff761f0ce1b81578299755a86
ehc-io <eduardo@ehc.io>
2019-07-10 13:31:53 -0500

    Added file (RUN deploy all machines step by step)

---
+++ playbooks/00_run_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ansible-playbook ./01_deploy_ubuntu_1804_LTS.yaml
+ansible-playbook ./02_deploy_bigip_1nic.yaml
+ansible-playbook ./03_update_security_group.yaml
+ansible-playbook -i ./azure_hosts ./04_deploy_vs.yaml
