ca6efa9c08819faea5e6aa172b46494260302ba5
ehc-io <eduardo@ehc.io>
2019-07-10 12:23:19 -0500

    fixed volume syntax

--- run_ansible_host.sh
+++ run_ansible_host.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -it --name azure-ansible -v ./playbooks:/root/azure \
-eduardoc/f5-ansible-container:azure
+docker run -it --name azure-ansible -v $PWD/playbooks:/root/azure eduardoc/f5-ansible-container:azure
