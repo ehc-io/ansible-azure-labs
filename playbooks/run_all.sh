d430a0778a2f7d44e497809fc6aefea660caf384
ehc-io <eduardo@ehc.io>
2019-07-19 17:55:35 -0300

    improved timing and added counter

--- playbooks/run_all.sh
+++ playbooks/run_all.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+#
+#
+SECONDS=0
+#
+echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 ansible-playbook ./01_deploy_ubuntu_1804_LTS.yaml
+#
+echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 ansible-playbook ./02_deploy_bigip_1nic.yaml
+#
+echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 ansible-playbook ./03_update_security_group.yaml
+#
+#
+sleep 30
+echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 ansible-playbook -i ./azure_hosts ./04_deploy_vs.yaml
-echo ""
+
+echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
+# total script execution time
+echo -e "$(date +'%Y-%d-%m %H:%M'): elapsed time:${RED} $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec${NC}"
+
+exit 0
