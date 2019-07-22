349c1e9f584db564d624dc0e3fc980b3f5c6bfad
ehc-io <eduardo@ehc.io>
2019-07-22 08:02:48 -0300

    added timing

--- playbooks/remove_all.sh
+++ playbooks/remove_all.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+#
+#
+SECONDS=0
+echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 ansible-playbook ./05_remove_rg.yaml
-echo ""
+# total script execution time
+echo -e "$(date +'%Y-%d-%m %H:%M'): elapsed time:${RED} $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec${NC}"
+#
+exit 0
