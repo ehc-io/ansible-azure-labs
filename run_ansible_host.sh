442bac5f0ffab2aa6eb55a414279b67a8194d88e
ehc-io <eduardo@ehc.io>
2019-07-22 04:15:14 -0300

    minor fixes

--- run_ansible_host.sh
+++ run_ansible_host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 
 # 
-docker run -it --name azure-ansible -v $PWD/playbooks:/root/azure eduardoc/f5-ansible-container:azure
+docker run -it -v $PWD/playbooks:/root/azure eduardoc/f5-ansible-container:azure
